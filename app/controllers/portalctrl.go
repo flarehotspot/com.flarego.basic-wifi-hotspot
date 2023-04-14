c10fc02ecf35df800342cda15dbae4a838f93561
Adones Pitogo <pitogo.adones@gmail.com>
2023-04-15 03:23:01 +0800

    refactor payments api

--- app/controllers/portalctrl.go
+++ app/controllers/portalctrl.go
@@ -1,9 +1,9 @@
 package controllers
 
 import (
-	curr "github.com/flarehotspot/sdk/api/currencies"
+	"github.com/flarehotspot/sdk/api/currencies"
 	"github.com/flarehotspot/sdk/api/models/device"
-	pymnt "github.com/flarehotspot/sdk/api/payments"
+	"github.com/flarehotspot/sdk/api/payments"
 	"github.com/flarehotspot/sdk/api/plugin"
 	"github.com/flarehotspot/sdk/api/web/contexts"
 	"github.com/flarehotspot/wifi-hotspot/app/routes/names"
@@ -20,22 +20,22 @@ func (ctrl *PortalCtrl) GetInsertCoin(w http.ResponseWriter, r *http.Request) {
 	if ok && device != nil {
 		log.Println("Insert coin device mac: ", device.MacAddress())
 
-		item := &pymnt.PaymentRequestItem{
+		item := &payments.PaymentRequestItem{
 			Sku:         "some-sku",
 			Name:        "Wifi Connection",
 			Description: "Purchase for wifi connection",
-			UnitAmount: &pymnt.UnitAmount{
-				CurrencyCode: curr.CurrencyPhilippinePeso,
-				Value:        11.0,
+			UnitAmount: &payments.UnitAmount{
+				CurrencyCode:   currencies.CurrencyPhilippinePeso,
+				VariableAmount: true,
 			},
 		}
 
-		params := &pymnt.PaymentRequestParams{
-			Items:       []*pymnt.PaymentRequestItem{item},
-			ReturnRoute: names.RoutePaymentReceived,
+		params := &payments.PaymentRequestParams{
+			Items:       []*payments.PaymentRequestItem{item},
+			CallbackUrl: ctrl.api.HttpApi().Router().UrlForRoute(names.RoutePaymentReceived),
 		}
 
-		ctrl.api.HttpApi().Respond().RequestPayment(w, r, params)
+		ctrl.api.PaymentsApi().RequestPayment(w, r, params)
 	} else {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
