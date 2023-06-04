0265e819e7cd4a02aa172943fad068390a03706b
Adones Pitogo <pitogo.adones@gmail.com>
2023-06-04 14:06:48 +0800

    sessions btn

--- app/web/navs/session-btn.go
+++ app/web/navs/session-btn.go
@@ -1,8 +1,11 @@
 package navs
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/flarehotspot/sdk/api/connmgr"
+	"github.com/flarehotspot/sdk/api/http/contexts"
 	"github.com/flarehotspot/sdk/api/http/navigation"
 	"github.com/flarehotspot/sdk/api/plugin"
 	"github.com/flarehotspot/wifi-hotspot/app/routes/names"
@@ -17,22 +20,44 @@ func NewSessionBtnNav(api plugin.IPluginApi, r *http.Request) *SessionBtnNav {
 	return &SessionBtnNav{api, r}
 }
 
-func (nav *SessionBtnNav) Handler(r *http.Request) navigation.IPortalItemData {
-	return NewSessionBtnNav(nav.api, r)
+func (self *SessionBtnNav) Handler(r *http.Request) navigation.IPortalItemData {
+	return NewSessionBtnNav(self.api, r)
 }
 
-func (nav *SessionBtnNav) IconPath() string {
+func (self *SessionBtnNav) IconPath() string {
 	return ""
 }
 
-func (nav *SessionBtnNav) Text() string {
-	return "Start Session"
+func (self *SessionBtnNav) Text() string {
+	clnt, err := self.client()
+	if err != nil {
+		return err.Error()
+	}
+
+	if clnt.IsConnected() {
+		return "Pause"
+	}
+
+	return "Connect"
 }
 
-func (nav *SessionBtnNav) Description() string {
+func (self *SessionBtnNav) Description() string {
 	return ""
 }
 
-func (nav *SessionBtnNav) Href() string {
-	return nav.api.Utils().UrlForRoute(names.RouteStartSession)
+func (self *SessionBtnNav) Href() string {
+	return self.api.Utils().UrlForRoute(names.RouteStartSession)
+}
+
+func (self *SessionBtnNav) client() (connmgr.IClientDevice, error) {
+	if self.r == nil {
+		return nil, errors.New("Session http request is not initialized.")
+	}
+
+	clntSym := self.r.Context().Value(contexts.ClientCtxKey)
+	clnt, ok := clntSym.(connmgr.IClientDevice)
+	if !ok {
+		return nil, errors.New("Could not determine client device.")
+	}
+	return clnt, nil
 }
