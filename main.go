74ecbbb46872ac133d0677225ecc2bd18994a5b3
Adones Pitogo <pitogo.adones@gmail.com>
2023-08-07 06:58:19 +0800

    Revert "using sdk api"
    This reverts commit 3f7769fc0c12fe598df666e4030ca81c964b486d.

--- main.go
+++ main.go
@@ -3,29 +3,17 @@
 package main
 
 import (
-	"log"
-
+	"github.com/flarehotspot/sdk/v1.0.0/api/plugin"
 	"github.com/flarehotspot/com.flarego.basic-wifi-hotspot/app/routes"
 	"github.com/flarehotspot/com.flarego.basic-wifi-hotspot/app/web/navs"
-	"github.com/flarehotspot/sdk"
-	"github.com/flarehotspot/sdk/v1.0.0/api"
-	"github.com/flarehotspot/sdk/v1.0.0/api/plugin"
 )
 
 func main() {}
 
-func Init(_sdk sdk.SDK) {
-	sym, err := _sdk.GetVersion(api.VERSION)
-	if err != nil {
-		log.Println("Unable to get plugin api: ", err)
-	}
+func Init(api plugin.IPluginApi) {
+	routes.PortalRoutes(api)
+	routes.AdminRoutes(api)
 
-	apiv1 := sym.(plugin.IPluginApi)
-
-	routes.PortalRoutes(apiv1)
-	routes.AdminRoutes(apiv1)
-
-	navs.SetPortalItems(apiv1)
-	navs.AdminWifiRates(apiv1)
-	log.Printf("Success loading plugin: %s", apiv1.Name())
+	navs.SetPortalItems(api)
+	navs.AdminWifiRates(api)
 }
