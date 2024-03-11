cbd06e3a4aa3005434c0df52a81823a68c8b5dfd
EternalTernity <30383305+EternalTernity@users.noreply.github.com>
2024-03-11 14:23:59 +0800

    Fixed calculating denominations not limited to 1,5, and 10.

--- app/controllers/utils.go
+++ app/controllers/utils.go
@@ -1,9 +1,27 @@
 package controllers
 
-func divideIntoTensFivesOnes(n int) (int, int, int) {
-	tens := n / 10
-	remainder := n % 10
-	fives := remainder / 5
-	ones := remainder % 5
-	return tens, fives, ones
+import "github.com/flarehotspot/com.flarego.basic-wifi-hotspot/app/utils"
+
+/*
+Calculates the breakdown of time and data based on the payment amount and the provided
+payment settings. It iterates over the payment settings in reverse order, starting from the highest
+denomination, and deducts the amount from the payment until it can't be deducted anymore. Then, it
+accumulates the time and data accordingly.
+*/
+func divideIntoTimeData(paymentAmount float64, paymentSettings utils.PaymentSettings) (float64, float64) {
+	var totalTime, totalData float64
+
+	for i := len(paymentSettings) - 1; i >= 0; i-- {
+		amount := paymentSettings[i].Amount
+		time := paymentSettings[i].TimeMins
+		data := paymentSettings[i].DataMb
+
+		for paymentAmount >= amount {
+			totalTime += time
+			totalData += data
+			paymentAmount -= amount
+		}
+	}
+
+	return totalTime, totalData
 }
