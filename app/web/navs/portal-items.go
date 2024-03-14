b8563530c87e8cd5fead57f3df7d62ef5f94f991
Tomshiet <30383305+EternalTernity@users.noreply.github.com>
2024-03-14 15:57:16 +0800

    "Appending items checking whether a client is running or has available session"

---
+++ app/web/navs/portal-items.go
@@ -0,0 +1,72 @@
+package navs
+
+import (
+	"net/http"
+
+	"github.com/flarehotspot/com.flarego.basic-wifi-hotspot/app/controllers"
+	"github.com/flarehotspot/com.flarego.basic-wifi-hotspot/app/utils"
+	sdkhttp "github.com/flarehotspot/sdk/api/http"
+	sdkpayments "github.com/flarehotspot/sdk/api/payments"
+	sdkplugin "github.com/flarehotspot/sdk/api/plugin"
+)
+
+func SetPortalItems(api sdkplugin.PluginApi) {
+	vrouter := api.Http().VueRouter()
+	vrouter.RegisterPortalRoutes([]sdkhttp.VuePortalRoute{
+		{
+			RouteName: "portal.insert-coin",
+			RoutePath: "/insert-coin",
+			Component: "portal/InsertCoin.vue",
+			HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+				p := sdkpayments.PurchaseRequest{
+					Sku:                  "wifi-connection",
+					Name:                 "WiFi Connection",
+					Description:          "Basic Wifi Hotspot",
+					AnyPrice:             true,
+					CallbackVueRouteName: "portal.purchase-callback",
+				}
+				api.Payments().Checkout(w, r, p)
+			},
+		},
+		{
+			RouteName:   "portal.purchase-callback",
+			RoutePath:   "/purchase-callback",
+			Component:   "portal/PurchaseCallback.vue",
+			HandlerFunc: controllers.PaymentRecevied(api),
+		},
+		{
+			RouteName:   "portal.start-session",
+			RoutePath:   "/start-session",
+			HandlerFunc: controllers.StartSession(api),
+		},
+		{
+			RouteName:   "portal.pause-session",
+			RoutePath:   "/pause-session",
+			HandlerFunc: controllers.PauseSession(api),
+		},
+	}...)
+
+	vrouter.PortalItemsFunc(func(r *http.Request) []sdkhttp.VuePortalItem {
+
+		navs := []sdkhttp.VuePortalItem{}
+
+		navs = append(navs, sdkhttp.VuePortalItem{
+			IconPath:  "images/wifi-logo.png",
+			Label:     "Insert Coin",
+			RouteName: "portal.insert-coin",
+		})
+
+		navs = append(navs, sdkhttp.VuePortalItem{
+			IconPath:  "images/wifi-logo.png",
+			Label:     utils.TextBtn(api, r),
+			RouteName: utils.SessionRoute(api, r),
+		})
+
+		if utils.TextBtn(api, r) == "" {
+			navs = append(navs[:1], navs[2:]...)
+		}
+
+		return navs
+	})
+
+}
